c02aa6cbd5a90246143f0b8a387e522fc098c586
Flaminia <flaminia.cavallo@essencedigital.com>
2014-09-25 14:07:20 +0100

    working version

--- drive-client.py
+++ drive-client.py
@@ -2,11 +2,13 @@ from apiclient.discovery import build
 from apiclient import errors
 from apiclient.http import MediaFileUpload
 import httplib2
-
 import auth
-
 import datetime
 import pprint
+import ConfigParser
+
+
+CONFIG = 'gdrive.ini'
 
 def build_service(credentials):
     """Build a Drive service object.
@@ -50,33 +52,75 @@ def upload_file(service, filepath, title, description, mimeType):
     file = service.files().insert(body=body, media_body=media_body).execute()
     return file
 
-def get_folder(service, folder_name):
-    query = "mimeType = 'application/vnd.google-apps.folder' and title = '{}'".format(folder_name)
+
+def get_folder(service, folder_name = None):
+    query = "mimeType = 'application/vnd.google-apps.folder'"
+    if folder_name:
+         query += "and title = '{}'".format(folder_name)
     folder = service.files().list(q=query).execute()
     return folder
 
-def get_folder_contents(service, folder, modified_since=None):
+
+def get_folder_files(service, folder, modified_since=None):
+    if len(folder['items']) < 1:
+        yield
     folder_id = folder['items'][0]['id']
     child_list_response = service.files().list(q="'{}' in parents".format(folder_id)).execute()
     children = child_list_response['items']
     for child in children:
         modified = datetime.datetime.strptime(child['modifiedDate'], '%Y-%m-%dT%H:%M:%S.%fZ')
         if (modified_since is None) or modified > modified_since:
-            yield child
+            yield child        
+            
+def get_file(service, file_name):
+    query = "mimeType != 'application/vnd.google-apps.folder' and title = '{}'".format(file_name)
+    file = service.files().list(q=query).execute()
+    return file['items']
 
+def download_xlsx(files, path):
+    for file in files:
+        pprint.pprint(file)
+        title = file['title']
+        download_url = file['exportLinks']['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']
+        with open(path + '/' + title + ".xlsx", 'w')  as fout:
+            response, content = service._http.request(download_url)
+            fout.write(content)
+    
+def read_config(fpath):
+    config = ConfigParser.SafeConfigParser()
+    config.read(fpath)
+    sprdsht = []
+    for sp_name in config.sections():
+        sp_details = {}
+        sp_details['job_name'] = sp_name
+        sp_details['type'] = config.get(sp_name, 'type')
+        sp_details['title'] = config.get(sp_name, 'title')
+        sp_details['folder'] = config.get(sp_name, 'folder')
+        try:
+            sp_details['modified_since'] = config.get(sp_name,'modified_since')
+        except ConfigParser.NoOptionError:
+            pass #optional
+        sprdsht.append(sp_details)
+    return sprdsht
 
 if __name__ == '__main__':
     creds = auth.get_stored_credentials('data@essencedigital.com')
     service = build_service(creds)
-    folder = get_folder(service, 'data@')
-    modified_since = datetime.datetime.strptime('2014-08-24', '%Y-%m-%d')
-    for child in get_folder_contents(service, folder, modified_since):
-        pprint.pprint(child)
-        title = child['title']
-        download_url = child['exportLinks']['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']
-        with open(title + ".xlsx", 'w')  as fout:
-            response, content = service._http.request(download_url)
-            fout.write(content)
-        #pprint.pprint(child['originalFilename'])
+    
+    config = read_config(CONFIG)
+    
+    files = []
+    for job in config:
+        if job['type'] == 'file':
+            file_name = job['title']
+            files = get_file(service,file_name)
+        elif job['type'] == 'folder':
+            folder_name = job['title']
+            folder = get_folder(service, folder_name)
+            date = job.get('modified_since',None)
+            modified_since = datetime.datetime.strptime(date, '%Y-%m-%d') if date else None
+            files = get_folder_contents(service, folder, modified_since)
+        download_xlsx(files, job['folder']) 
+
 
 
